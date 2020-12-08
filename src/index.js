a049957207162bfe4a0189508d5626d7c8be2199
aditimodgil <modgiladiti@gmail.com>
2020-12-08 17:25:30 +0530

    add Provider in index js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import {store} from './createStore'; 
 
 ReactDOM.render(
+  <Provider store={store}>
     <App />
-  ,
+  </Provider>,
   document.getElementById('root')
 );
 
